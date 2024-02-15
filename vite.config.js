a1f8c51c6c9e40755bbae484dda643172a598ffc
zubiadil999 <zubiadil9@gmail.com>
2024-02-15 20:17:23 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: 'dist' // Specify the output directory here
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-            distPath: 'dist' // Specify the output directory here
+            refresh: true
         }),
     ],
 });
